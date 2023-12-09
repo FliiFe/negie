d4bd3a9005ee082b8eafb5001b5943cc05185ad1
Théophile Cailliau <theophile.cailliau@gmail.com>
2023-12-09 20:35:50 +0100

    1:1 pic with circle marks

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate image;
+extern crate imageproc;
 
 extern crate nalgebra as na;
 extern crate nalgebra_lapack as nal;
@@ -8,26 +9,32 @@ use nal::Eigen;
 use rand::{seq::SliceRandom, Rng};
 use image::{RgbImage, Rgb};
 
+use std::time::{Instant, Duration};
+
+
 fn main() {
     let dim = 5;
     let n_ts = 2;
     let nsamples = 100000;
     let pop = vec![Complex::i(),
                     -Complex::i(),
-                    Complex::from_real(0.),
-                    Complex::from_real(1.),
-                    Complex::from_real(0.5),
+                    // Complex::from_real(0.),
+                    // Complex::from_real(1.),
+                    // Complex::from_real(0.2),
                     ];
     let matrix = random_matrix(dim, pop);
     let variable_indices = random_indices_distinct_variables(dim, n_ts);
-    let samples = sample_ts(nsamples, n_ts, Distribution::UniformUnits);
+    let samples = sample_ts(nsamples, n_ts, Distribution::UniformRects { r: 20. });
+    let start = Instant::now();
     let egvs = collect_eigenvalues(&matrix, &variable_indices, n_ts, &samples);
+    println!("duration {:?}", start.elapsed());
     println!("{}", matrix);
     println!("{:?}", egvs.len());
-    save_eigen_image(egvs, 2000, 2000);
+    save_eigen_image(egvs, nsamples, 2000, 2000);
 }
 
-fn save_eigen_image(eigenvalues: Vec<Complex<f64>>, width: u32, height: u32) {
+
+fn save_eigen_image(eigenvalues: Vec<Complex<f64>>, nsamples: usize, width: u32, height: u32) {
     let mut img = RgbImage::new(width, height);
     for x in 0..width {
         for y in 0..height {
@@ -37,14 +44,20 @@ fn save_eigen_image(eigenvalues: Vec<Complex<f64>>, width: u32, height: u32) {
     let (xmin, xmax, ymin, ymax) = eigenvalues.iter().fold((0.,0.,0.,0.), |(xmin, xmax, ymin, ymax), z| {
         (f64::min(xmin, z.re), f64::max(xmax, z.re), f64::min(ymin, z.im), f64::max(ymax, z.im))
     });
+    let (min, max) = (xmin.min(ymin), xmax.max(ymax));
     let margin = width.max(height) / 20;
+    let radius = 50. * (width as f64 / (nsamples as f64));
+    println!("radius: {}", radius);
     for egv in eigenvalues.iter() {
-        img.put_pixel(
-            round(clamp(egv.re, xmin, xmax, margin as f64, (width - margin) as f64)),
-            round(clamp(egv.im, ymin, ymax, margin as f64, (height - margin) as f64)),
+        imageproc::drawing::draw_filled_circle_mut(&mut img,
+            (round(clamp(egv.re, min, max, margin as f64, (width - margin) as f64)).try_into().unwrap(),
+            round(clamp(egv.im, min, max, margin as f64, (height - margin) as f64)).try_into().unwrap()),
+            round(radius).try_into().unwrap(),
             Rgb([56, 59, 62]));
     }
+    print!("Saving picture...");
     img.save("/tmp/test.png").expect("Couldn't save image");
+    println!("DONE");
 }
 
 fn round(x: f64) -> u32 {
@@ -93,6 +106,15 @@ fn sample_ts(ns: usize, n_ts: usize, distrib: Distribution) -> Vec<Vec<Complex<f
                     (0..n_ts).map(|_| { 
                         let phase = rng.gen_range((0.0_f64)..(f64::two_pi()));
                         Complex::new(phase.cos(), phase.sin())
+                    }).collect()
+                )
+            }
+        }
+        Distribution::UniformRects { r } => {
+            for _ in 0..ns {
+                samples.push(
+                    (0..n_ts).map(|_| { 
+                        Complex::new(rng.gen_range(-r..r),0.)
                     }).collect()
                 )
             }
